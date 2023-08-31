75bda4267a7c220a3836bf8aa2a0a3c608d8ca19
Makima <122879488+Conny210@users.noreply.github.com>
2023-09-01 00:00:48 +0200

    updated index.js

--- index.js
+++ index.js
@@ -1,41 +1,110 @@
-function cityName(event) {
+// Format timestamp to a readable date and time string
+function currentDate(time) {
+  const date = new Date(time);
+  const formattedHours = (date.getHours() < 10 ? "0" : "") + date.getHours();
+  const formattedMinutes =
+    (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayOfWeek = daysOfWeek[date.getDay()];
+  return `${dayOfWeek} ${formattedHours}:${formattedMinutes}`;
+}
+
+// Display weather data on the webpage
+function displayTemperature(response) {
+  const temperatureElement = document.querySelector("#temperature");
+  const cityElement = document.querySelector("#city");
+  const descriptionElement = document.querySelector("#description");
+  const humidityElement = document.querySelector("#humidity");
+  const windElement = document.querySelector("#wind");
+  const dateElement = document.querySelector("#date");
+  const iconElement = document.querySelector("#icon");
+
+  const celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  const iconUrl = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+  iconElement.setAttribute("src", iconUrl);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+}
+
+// Fetch weather data for a given city
+function fetchWeather(city) {
+  const units = "metric";
+  const apiKey = "73aa23218a979a57aac07ba4ab59beb9";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+// Handle form submission
+function handleSubmit(event) {
   event.preventDefault();
-  let input = document.querySelector("#newCity");
-  let city = document.querySelector("#city");
-  city.innerHTML = input.value;
-
-  let newCity = input.value;
-  let units = "metric";
-  let apiKey = "73aa23218a979a57aac07ba4ab59beb9";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=${units}&appid=${apiKey}`;
-  axios.get(url).then(showTemp);
+  const cityInputElement = document.querySelector("#newCity");
+  fetchWeather(cityInputElement.value);
 }
 
-let form = document.querySelector("form");
-form.addEventListener("submit", cityName);
-
-function showTemp(response) {
-  let temp = Math.round(response.data.main.temp);
-  let newTemp = document.querySelector("#temp");
-  newTemp.innerHTML = temp;
+// Convert temperature to Fahrenheit
+function fahrenheitTemp(event) {
+  event.preventDefault();
+  const temperatureElement = document.querySelector("#temp");
+  const fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+  celsius.classList.remove("active");
+  fahrenheit.classList.add("active");
 }
 
-///
+// Convert temperature back to Celsius
+function celsiusTemp(event) {
+  event.preventDefault();
+  const temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+  celsius.classList.add("active");
+  fahrenheit.classList.remove("active");
+}
+
+// Global variables
+let celsiusTemperature = null;
+const form = document.querySelector("#search");
+const tempFahrenheit = document.querySelector("#fahrenheit");
+const tempCelsius = document.querySelector("#celsius");
+
+// Event listeners
+form.addEventListener("submit", handleSubmit);
+tempFahrenheit.addEventListener("click", displayFahrenheitTemperature);
+tempCelsius.addEventListener("click", displayCelsiusTemperature);
+
+// Initial search
+search("Randburg");
+
+// Current location button
 function clickedButton() {
   function getPosition(position) {
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-    let lat = position.coords.latitude;
-    let lon = position.coords.longitude;
-    let units = "metric";
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const units = "metric";
+    const apiKey = "73aa23218a979a57aac07ba4ab59beb9";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
 
     function showLocationTemp(response) {
-      let locTemp = Math.round(response.data.main.temp);
-      let newLocTemp = document.querySelector("#temp");
+      const locTemp = Math.round(response.data.main.temp);
+      const newLocTemp = document.querySelector("#temp");
       newLocTemp.innerHTML = locTemp;
 
-      let currentCity = response.data.name;
-      let currentCityLoc = document.querySelector("#city");
+      const currentCity = response.data.name;
+      const currentCityLoc = document.querySelector("#city");
       currentCityLoc.innerHTML = currentCity;
     }
 
@@ -44,11 +113,8 @@ function clickedButton() {
       .then(showLocationTemp);
   }
 
-  let apiKey = "73aa23218a979a57aac07ba4ab59beb9";
-  let apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
-
   navigator.geolocation.getCurrentPosition(getPosition);
 }
 
-let element = document.querySelector("button");
+const element = document.querySelector("button");
 element.addEventListener("click", clickedButton);
