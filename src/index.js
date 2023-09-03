426f7ef12866a9643f0eabe16d914d68314eccba
Makima <122879488+Conny210@users.noreply.github.com>
2023-09-03 20:07:24 +0200

    Updated .js script

--- src/index.js
+++ src/index.js
@@ -13,8 +13,15 @@ let day = now.getDay();
 let currentDay = days[day];
 
 let hour = now.getHours();
+ if (hour < 10) {
+   hour = `0${hour}`;
+ }
 
 let minutes = now.getMinutes();
+ if (minutes < 10) {
+   minutes = `0${minutes}`;
+ }
+ 
 let dateTime = `${currentDay} ${hour}:${minutes}`;
 
 let date = document.querySelector("#date");
